04092c383ff50d7d6869bcab6bf77f4dcae0d3e8
Simone <armari.simone@gmail.com>
2017-09-08 10:19:02 +0200

    Lettura della struttura completa

--- myFunction.py
+++ myFunction.py
@@ -1,7 +1,14 @@
 #Funzione che stampa una tabella completa di struttura
 def printTable(conn, name):
 
+    # Connessione al database per ottenere l'informazione sulla tabella
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='" + name +"'")
-    #for row in cursor:
-    print('row = %r' % (cursor))
+    cursor.execute("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME='" + name + "'")
+
+    # Stampa della cornice superiore
+
+    # Stampa della struttura
+    print("|", end="")
+    for row in cursor:
+        #print("         %s          |" % (row[3]), end="")
+        print("%s" % (row))
