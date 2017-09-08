b339eeb60b7d2085802505cb0c926f8bda87446d
Simone <armari.simone@gmail.com>
2017-09-08 17:25:06 +0200

    Corretta uscita dal ciclo

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ choose = input()
 if choose == "1":
 
     # Ripeto finchè la scelta non è 8
-    while choose != 8:
+    while choose != "8":
         print("             MENU INSERIMENTO  ")
         print(30 * '-')
         print("1. Inserisci docente")
