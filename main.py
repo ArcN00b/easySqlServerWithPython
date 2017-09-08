6c7c938c87fb7282df81f76819db84a38e7aed6d
Simone <armari.simone@gmail.com>
2017-09-08 09:58:17 +0200

    Commit iniziale

---
+++ main.py
@@ -0,0 +1,14 @@
+import pyodbc
+
+conn = pyodbc.connect("Driver={SQL Server Native Client 11.0};" "Server=localhost\SQLEXPRESS;" "Database=scuola;" "Trusted_Connection=yes;")
+cursor=conn.cursor()
+'''try:
+    cursor.execute("INSERT INTO Studente (Nome, Cognome, Indirizzo, Cap, Tel, Nascita) "
+               "VALUES ('Maria' , 'Bianchi', 'Via L. Ariosto 9, Ferrara (FE)', '44121', '3483483483', '19921123')")
+    conn.commit()
+except:
+    conn.rollback()'''
+cursor.execute("SELECT * FROM INFORMATION.SCHEMA WHERE TABLE_NAME=Studente")
+for row in cursor: print('row = %r' % (row,))
+
+conn.close()
