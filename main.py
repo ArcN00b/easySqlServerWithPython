76e46f0b9de9e8342117e04d50231ec0ca443542
Simone <armari.simone@gmail.com>
2017-09-09 11:13:25 +0200

    Modificata tabella Esame, ora si collega a Gruppo Aggiunti campi per inserimento e modifica nuovo esame

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ print("1. Inserimento")
 print("2. Visualizzazione")
 print("3. Cancellazione")
 print("4. Modifica")
+print("5. Ricerca")
 print(30 * '-')
 
 # Input della scelta
@@ -81,14 +82,15 @@ if choose == "1":
             myFunction.insertInto(conn, "Studente", attributi, valori)
 
         #Inserimento nuovo esame
-        #TODO aggiunta di collegamento tra esame e materia
         if choose == "3":
 
             # Stampo gli studenti già presenti
             myFunction.printTable(conn, "Studente")
+            myFunction.printTable(conn, "Gruppo")
+            myFunction.printTable(conn, "Esame")
 
             # Richiedo i dati e li preparo per la query
-            campi = ["Voto", "Lode", "Data", "Matricola"]
+            campi = ["Voto", "Lode", "Data", "Tipo", "Matricola", "Gruppo"]
             valori = []
             for n in campi:
                 print(n + " = ", end="")
@@ -99,7 +101,7 @@ if choose == "1":
                 valori.append(temp)
 
             # Eseguo la query di inserimento
-            attributi = "Voto, Lode, Data, Matricola_Stud"
+            attributi = "Voto, Lode, Data, Tipo, Matricola_Stud, ID_Gruppo"
             myFunction.insertInto(conn, "Esame", attributi, valori)
 
         # Inserimento nuovo tipo di esercitazione
@@ -303,6 +305,227 @@ if choose == "4":
     print("   MENU MODIFICA ")
     print(30 * '-')
     print("1. Modifica docente")
+    print("2. Modifica studente")
+    print("3. Modifica un esame svolto")
+    print("4. Modifica un tipo di esercitazione")
+    print("5. Modifica un programma d'esame")
+    print("6. Modifica un gruppo di esercitazioni")
+    print("7. Modifica una partecipazione ai gruppi")
+    print("8. Torna al menu precedente")
+
+    # Input della scelta
+    choose = input()
+
+    # Cancellazione scelta
+    if choose == "1":
+
+        # Chiedo la matricola in input
+        myFunction.printTable(conn, "Docente")
+        print("Selezionare la matricola da modificare ", end="")
+        matricola = input()
+
+        # Controllo che la matricola inserita esista
+        if myFunction.check(conn, "Docente", "Matricola", matricola):
+
+            # Richiedo tutti gli altri campi
+            campi = ["Nome", "Cognome", "Indirizzo", "Cap", "Tel", "Data di nascita", "Dipartimento"]
+            valori = []
+            for n in campi:
+                print(n + " = ", end="")
+                temp = input()
+                while len(temp) == 0:
+                    print(n + " = ", end="")
+                    temp = input()
+                valori.append(temp)
+
+            # Eseguo la funzione di aggiornamento
+            attributi = ["Nome", "Cognome", "Indirizzo", "Cap", "Tel", "Nascita", "Dipartimento"]
+            myFunction.update(conn, "Docente", attributi, valori, "Matricola = '" + matricola + "'")
+
+        # Se la matricola non esiste scrivo messaggio d'errore
+        else:
+            print("La matricola scelta non esiste")
+    if choose == "2":
+
+        # Chiedo la matricola in input
+        myFunction.printTable(conn, "Studente")
+        print("Selezionare la matricola da modificare ", end="")
+        matricola = input()
+
+        # Controllo che la matricola inserita esista
+        if myFunction.check(conn, "Studente", "Matricola", matricola):
+
+            # Richiedo tutti gli altri campi
+            campi = ["Nome", "Cognome", "Indirizzo", "Cap", "Tel", "Data di nascita"]
+            valori = []
+            for n in campi:
+                print(n + " = ", end="")
+                temp = input()
+                while len(temp) == 0:
+                    print(n + " = ", end="")
+                    temp = input()
+                valori.append(temp)
+
+            # Eseguo la funzione di aggiornamento
+            attributi = ["Nome", "Cognome", "Indirizzo", "Cap", "Tel", "Nascita"]
+            myFunction.update(conn, "Studente", attributi, valori, "Matricola = '" + matricola + "'")
+
+        # Se la matricola non esiste scrivo messaggio d'errore
+        else:
+            print("La matricola scelta non esiste")
+    if choose == "3":
+
+        # Chiedo l'id in input
+        myFunction.printTable(conn, "Esame")
+        print("Selezionare l'id da modificare ", end="")
+        id = input()
+
+        # Controllo che l'id inserito esista
+        if myFunction.check(conn, "Esame", "ID", id):
+
+            # Richiedo tutti gli altri campi
+            campi = ["Voto", "Lode", "Data", "Tipo", "Matricola", "Gruppo"]
+            valori = []
+            for n in campi:
+                print(n + " = ", end="")
+                temp = input()
+                while len(temp) == 0:
+                    print(n + " = ", end="")
+                    temp = input()
+                valori.append(temp)
+
+            # Eseguo la funzione di aggiornamento
+            attributi = ["Voto", "Lode", "Data", "Tipo", "Matricola_Stud", "ID_Gruppo"]
+            myFunction.update(conn, "Esame", attributi, valori, "ID = '" + id + "'")
+
+        # Se l'id non esiste scrivo messaggio d'errore
+        else:
+            print("L'id scelto non esiste")
+    if choose == "4":
+
+        # Chiedo l'id in input
+        myFunction.printTable(conn, "Tipo")
+        print("Selezionare l'id da modificare ", end="")
+        id = input()
+
+        # Controllo che l'id inserito esista
+        if myFunction.check(conn, "Tipo", "ID", id):
+
+            # Richiedo tutti gli altri campi
+            campi = ["Tipo", "Frontale"]
+            valori = []
+            for n in campi:
+                print(n + " = ", end="")
+                temp = input()
+                while len(temp) == 0:
+                    print(n + " = ", end="")
+                    temp = input()
+                valori.append(temp)
+
+            # Eseguo la funzione di aggiornamento
+            attributi = ["Tipo", "Frontale"]
+            myFunction.update(conn, "Tipo", attributi, valori, "ID = '" + id + "'")
+
+        # Se l'id non esiste scrivo messaggio d'errore
+        else:
+            print("L'id scelto non esiste")
+    if choose == "5":
+
+        # Chiedo l'id in input
+        myFunction.printTable(conn, "Programma")
+        print("Selezionare l'id da modificare ", end="")
+        id = input()
+
+        # Controllo che l'id inserito esista
+        if myFunction.check(conn, "Programma", "ID", id):
+
+            # Richiedo tutti gli altri campi
+            campi = ["Descrizione", "Bibliografia"]
+            valori = []
+            for n in campi:
+                print(n + " = ", end="")
+                temp = input()
+                while len(temp) == 0:
+                    print(n + " = ", end="")
+                    temp = input()
+                valori.append(temp)
+
+            # Eseguo la funzione di aggiornamento
+            attributi = ["Descrizione", "Bibliografia"]
+            myFunction.update(conn, "Programma", attributi, valori, "ID = '" + id + "'")
+
+        # Se l'id non esiste scrivo messaggio d'errore
+        else:
+            print("L'id scelto non esiste")
+    if choose == "6":
+
+        # Stampo i gruppi, i docenti, i tipi di esercitazione e i programmi d'esame
+        myFunction.printTable(conn, "Docente")
+        myFunction.printTable(conn, "Tipo")
+        myFunction.printTable(conn, "Programma")
+        myFunction.printTable(conn, "Gruppo")
+        print("Selezionare l'id del gruppo da modificare ", end="")
+        id = input()
+
+        # Controllo che l'id inserito esista
+        if myFunction.check(conn, "Gruppo", "ID", id):
+
+            # Richiedo tutti gli altri campi
+            campi = ["Orario", "Anno Accademico", "Tipo Esercitazione", "Matricola Docente", "Programma"]
+            valori = []
+            for n in campi:
+                print(n + " = ", end="")
+                temp = input()
+                while len(temp) == 0:
+                    print(n + " = ", end="")
+                    temp = input()
+                valori.append(temp)
+
+            # Eseguo la funzione di aggiornamento
+            attributi = ["Orario", "Anno_Accademico", "ID_Tipo", "Matricola_Doc", "ID_Pro"]
+            myFunction.update(conn, "Gruppo", attributi, valori, "ID = '" + id + "'")
+
+        # Se l'id non esiste scrivo messaggio d'errore
+        else:
+            print("L'id scelto non esiste")
+    if choose == "7":
+
+        # Stampo le partecipazioni, gli studenti e i gruppi
+        myFunction.printTable(conn, "Studente")
+        myFunction.printTable(conn, "Gruppo")
+        myFunction.printTable(conn, "Partecipa")
+        print("Selezionare l'id della partecipazione da modificare ", end="")
+        id = input()
+
+        # Controllo che l'id inserito esista
+        if myFunction.check(conn, "Partecipa", "ID", id):
+
+            # Richiedo tutti gli altri campi
+            campi = ["Matricola", "Gruppo"]
+            valori = []
+            for n in campi:
+                print(n + " = ", end="")
+                temp = input()
+                while len(temp) == 0:
+                    print(n + " = ", end="")
+                    temp = input()
+                valori.append(temp)
+
+            # Eseguo la funzione di aggiornamento
+            attributi = ["Matricola_Stud", "ID_Gruppo"]
+            myFunction.update(conn, "Partecipa", attributi, valori, "ID = '" + id + "'")
+
+        # Se l'id non esiste scrivo messaggio d'errore
+        else:
+            print("L'id scelto non esiste")
+    if choose == "8":
+        os.system("cls")
+
+# Menù Ricerca
+if choose == "5":
+    print("   MENU MODIFICA ")
+    print(30 * '-')
+    print("1. Visualizza gli esami sostenuti da uno studente")
     print("2. Modifica studente")
     print("3. Modifica un esame svolto")
     print("4. Modifica un tipo di esercitazione")
